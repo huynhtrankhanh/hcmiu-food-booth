b0d2396aecafaf4024b374b1e1f9209fe7689182
Huỳnh Trần Khanh <qcdz9r6wpcbh59+github@gmail.com>
2023-12-25 20:16:45 +0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,14 +1,26 @@
 import sqlite3
 from flask import Flask, request, jsonify
+from flask_httpauth import HTTPBasicAuth
+from passlib.hash import argon2
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 # Database Connection
-conn = sqlite3.connect('com_viet.db')
+conn = sqlite3.connect('com_viet.db', check_same_thread=False)
 c = conn.cursor()
+
+# Create tables
+c.execute('''CREATE TABLE IF NOT EXISTS users
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              username TEXT UNIQUE NOT NULL,
+              password_hash TEXT NOT NULL)''')
+
 c.execute('''CREATE TABLE IF NOT EXISTS orders
              (order_id INTEGER PRIMARY KEY AUTOINCREMENT,
-              status TEXT DEFAULT 'active')''')
+              user_id INTEGER,
+              status TEXT DEFAULT 'active',
+              FOREIGN KEY(user_id) REFERENCES users(id))''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS chosen_dishes
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,25 +30,69 @@ c.execute('''CREATE TABLE IF NOT EXISTS chosen_dishes
               FOREIGN KEY(order_id) REFERENCES orders(order_id))''')
 conn.commit()
 
-# Endpoints for choosing dishes
+@auth.verify_password
+def verify_password(username, password):
+    c.execute('SELECT password_hash FROM users WHERE username = ?', (username,))
+    user = c.fetchone()
+    if user and argon2.verify(password, user[0]):
+        return username
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'message': 'Missing username or password'}), 400
+
+    c.execute('SELECT id FROM users WHERE username = ?', (username,))
+    if c.fetchone():
+        return jsonify({'message': 'Username already exists'}), 409
+
+    password_hash = argon2.hash(password)
+    c.execute('INSERT INTO users (username, password_hash) VALUES (?, ?)', (username, password_hash))
+    conn.commit()
+    return jsonify({'message': 'User created successfully'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    auth_data = request.authorization
+    if not auth_data or not auth_data.username or not auth_data.password:
+        return jsonify({'message': 'Could not verify'}), 401
+
+    if verify_password(auth_data.username, auth_data.password):
+        return jsonify({'message': 'Logged in successfully'})
+    return jsonify({'message': 'Bad credentials'}), 401
+
 @app.route('/choose-dish', methods=['POST'])
+@auth.login_required
 def choose_dish():
     data = request.json
     dish_name = data.get('dishName')
     price = data.get('price')
-    order_id = get_active_order_id()
+    username = auth.current_user()
     
+    c.execute('SELECT id FROM users WHERE username = ?', (username,))
+    user_id = c.fetchone()[0]
+
+    order_id = get_active_order_id(user_id)
     if not order_id:
-        create_order()
+        create_order(user_id)
 
     c.execute('INSERT INTO chosen_dishes (order_id, dish_name, price) VALUES (?, ?, ?)', (order_id, dish_name, price))
     conn.commit()
     return jsonify({'message': 'Dish chosen successfully'})
 
-# Endpoint for reviewing shopping cart
 @app.route('/review-cart', methods=['GET'])
+@auth.login_required
 def review_cart():
-    order_id = get_active_order_id()
+    username = auth.current_user()
+    
+    c.execute('SELECT id FROM users WHERE username = ?', (username,))
+    user_id = c.fetchone()[0]
+
+    order_id = get_active_order_id(user_id)
     if order_id:
         c.execute('SELECT * FROM chosen_dishes WHERE order_id=?', (order_id,))
         cart_items = [{'id': row[0], 'dishName': row[2], 'price': row[3]} for row in c.fetchall()]
@@ -44,17 +100,22 @@ def review_cart():
     else:
         return jsonify({'message': 'No active order'})
 
-# Endpoint for deleting a dish from cart
 @app.route('/delete-dish/<int:dish_id>', methods=['DELETE'])
+@auth.login_required
 def delete_dish(dish_id):
     c.execute('DELETE FROM chosen_dishes WHERE id=?', (dish_id,))
     conn.commit()
     return jsonify({'message': 'Dish deleted successfully'})
 
-# Endpoint for calculating total
 @app.route('/calculate-total', methods=['GET'])
+@auth.login_required
 def calculate_total():
-    order_id = get_active_order_id()
+    username = auth.current_user()
+    
+    c.execute('SELECT id FROM users WHERE username = ?', (username,))
+    user_id = c.fetchone()[0]
+
+    order_id = get_active_order_id(user_id)
     if order_id:
         c.execute('SELECT SUM(price) FROM chosen_dishes WHERE order_id=?', (order_id,))
         total = c.fetchone()[0]
@@ -62,33 +123,12 @@ def calculate_total():
     else:
         return jsonify({'message': 'No active order'})
 
-# Additional endpoints related to suspicious behavior detection
-@app.route('/abuse', methods=['GET'])
-def detect_abuse():
-    # Implement logic to detect suspicious behavior
-    # For example, checking for abnormal order frequency or total amount spent
-    # Return relevant information about the suspicious behavior
-    return jsonify({'message': 'Suspicious behavior detected'})
-
-@app.route('/checkpoint/956', methods=['POST'])
-def report_checkpoint_956():
-    # Capture and report specific checkpoint event, e.g., user reaching a specific stage in the app
-    # Log the event and report back a success message
-    return jsonify({'message': 'Checkpoint 956 reported'})
-
-@app.route('/checkpoint/282', methods=['GET'])
-def get_checkpoint_282():
-    # Retrieve specific checkpoint details, if available
-    # May involve querying the database or other data source
-    checkpoint_data = {'name': 'Checkpoint 282', 'description': 'User reached a critical stage'}
-    return jsonify({'checkpoint': checkpoint_data})
-
-def create_order():
-    c.execute('INSERT INTO orders DEFAULT VALUES')
+def create_order(user_id):
+    c.execute('INSERT INTO orders (user_id) VALUES (?)', (user_id,))
     conn.commit()
 
-def get_active_order_id():
-    c.execute('SELECT order_id FROM orders WHERE status="active"')
+def get_active_order_id(user_id):
+    c.execute('SELECT order_id FROM orders WHERE status="active" AND user_id = ?', (user_id,))
     result = c.fetchone()
     return result[0] if result else None
 
